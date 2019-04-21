a12c031464ce5cd7c0c6172e131609f24acdf986
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-21 15:48:32 -0300

    Utilizando o objeto ContaCorrente

--- ByteBank/01-ByteBank/Program.cs
+++ ByteBank/01-ByteBank/Program.cs
@@ -10,7 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaDoCaue = new ContaCorrente();
+
+            contaDoCaue.titular = "Cauê Nishijima";
+            contaDoCaue.agencia = 3095;
+            contaDoCaue.numero = 084426;
+            contaDoCaue.saldo = 5000;
+
+            Console.WriteLine("Titular: " + contaDoCaue.titular);
+            Console.WriteLine("Agência: " + contaDoCaue.agencia);
+            Console.WriteLine("Número: " + contaDoCaue.numero);
+            Console.WriteLine("Saldo: " + contaDoCaue.saldo);
+
+            contaDoCaue.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDoCaue.saldo);
 
             Console.ReadLine();
         }
