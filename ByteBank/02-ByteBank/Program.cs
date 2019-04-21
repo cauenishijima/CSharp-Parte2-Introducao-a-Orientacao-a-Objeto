ccbba13526590b3475883b8e6117e73f75fe74b5
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-21 18:42:48 -0300

    Valores Padrões
    Valores dos atributos das classes quando usados sem receber nenhum valor possuem valores padrões, e esses valores padrões podem ser alterados diretamente na classe.

---
+++ ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Cauê Nishijima";
+
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
